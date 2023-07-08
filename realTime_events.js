29a681dc22242f35dd9b067ff3e8c7b3af5b9eb5
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-07-08 15:17:41 -0600

    Using Websocket protocol

---
+++ realTime_events.js
@@ -0,0 +1,25 @@
+
+
+
+let socket = new WebSocket('wss://localhost:80');
+let mensajeIngresado = document.getElementById('mensajeIngresado');
+let botonEnviar = document.getElementById('botonEnviar');
+
+function mostrarMensajes(contenido){
+    let contenedorMensajes = document.getElementById('mensajeChat');
+    let elementoMensaje = document.createElement('p');
+
+    elementoMensaje.innerHTML = contenido;
+    contenedorMensajes.appendChild(elementoMensaje);
+}
+
+botonEnviar.onclick = function() {
+    let mensaje = mensajeIngresado.value;
+    mostrarMensajes(mensaje);
+    socket.send(mensaje);
+};
+
+socket.onmessage = function(event) {
+    let mensaje = event.data;
+    mostrarMensajes(mensaje);
+}
